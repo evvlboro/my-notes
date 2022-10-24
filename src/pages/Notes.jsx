d31d37c26fc6016cbfa00d9fd992115841a66ef1
evvlboro <be@bchc.app>
2022-10-24 20:23:02 +0300

    feat: dynamic pagination

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import NoteList from '../components/NoteList';
-import {data} from '../API/NoteServise';
 import MyModal from '../components/UI/MyModal/MyModal';
 import MyButton from '../components/UI/MyButton/MyButton';
 import AddForm from '../components/AddForm';
 
+import axios from "axios";
+
 function Notes() {
     const [notes, setNotes] = React.useState([]);
     const [addModal, setAddModal] = React.useState(false);
+    const [currentPage, setCurrentPage] = React.useState(1);
+    const [fetching, setFetching] = React.useState(true);
+    const [totalCount, setTotalCount] = React.useState(0);
 
     React.useEffect(() => {
-        setNotes(data);
-    }, []);
+        if (fetching) {
+            axios.get(`http://localhost:3000/data?_limit=10&_page=${currentPage}`)
+                .then(res => {
+                    setNotes([...notes, ...res.data]);
+                    setCurrentPage(pervState => pervState + 1);
+                    setTotalCount(res.headers['x-total-count']);
+                })
+                .finally(() => {
+                    setFetching(false);
+                });
+        }
+    }, [fetching]);
 
     const createNote = (newNote) => {
         setNotes([newNote, ...notes ]);
@@ -22,6 +36,22 @@ function Notes() {
         setNotes(notes.filter(n => n.id !== note.id));
     }
 
+    const scrollHandler = (event) => {
+        if (event.target.documentElement.scrollHeight - (event.target.documentElement.scrollTop + window.innerHeight) < 100 
+            && notes.length < totalCount
+        ) {
+            setFetching(true);
+        }
+    }
+
+    React.useEffect(() => {
+        document.addEventListener('scroll', scrollHandler);
+
+        return function() {
+            document.removeEventListener('scroll', scrollHandler);
+        }
+    });
+
     return (
         <main className="main">
             <MyButton onClick={() => setAddModal(true)}>
@@ -30,7 +60,6 @@ function Notes() {
             <MyModal visible={addModal} setVisible={setAddModal}>
                 <AddForm create={createNote} />
             </MyModal>
-            
             <NoteList notes={notes} setaAddModal={setAddModal} remove={removeNote}/>
         </main>
     );
